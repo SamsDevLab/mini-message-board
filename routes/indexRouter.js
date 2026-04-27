959b5e0bb9843254288b2f93d4af128b4a09c801
Sam Hadfield <sthtech@protonmail.com>
2026-04-27 17:16:39 -0500

    feat: add 'view' to each msg, set up route
    Add 'view' to each msg so that user can see details for each specific
    message. Set up routing through indexRouter

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,11 +3,13 @@ const messages = [
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
+    id: crypto.randomUUID(),
   },
   {
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
+    id: crypto.randomUUID(),
   },
 ];
 
@@ -15,7 +17,7 @@ const express = require("express");
 const indexRouter = express.Router();
 
 indexRouter.get("/new", (req, res) => {
-  res.render("form", { messages: messages });
+  res.render("form.ejs", { messages: messages });
 });
 
 indexRouter.post("/new", (req, res) => {
@@ -24,13 +26,22 @@ indexRouter.post("/new", (req, res) => {
     text: formValues.messageText,
     user: formValues.authorName,
     added: new Date(),
+    id: crypto.randomUUID(),
   });
 
   res.redirect("/");
 });
 
+indexRouter.get("/message-details/:id", (req, res) => {
+  const requestedId = req.params.id;
+
+  const message = messages.find((message) => message.id === requestedId);
+
+  res.render("message-details", { message });
+});
+
 indexRouter.get("/", (req, res) => {
-  res.render("index", { title: "Mini Messageboard", messages: messages });
+  res.render("index.ejs", { title: "Mini Messageboard", messages: messages });
 });
 
 module.exports = indexRouter;
