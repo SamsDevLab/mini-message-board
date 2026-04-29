5875bd3faa9be5f4572e152515e8c89282554564
Sam Hadfield <sthtech@protonmail.com>
2026-04-29 09:32:04 -0500

    feat: add express.static to serve static files

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const path = require("node:path");
 const express = require("express");
 const app = express();
 const indexRouter = require("./routes/indexRouter");
 
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
