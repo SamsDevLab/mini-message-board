510b5069d2be8cca3efd30896739d99a702fa836
Sam Hadfield <sthtech@protonmail.com>
2026-05-08 08:53:11 -0500

    refactor: increase delay for getAllUsers()

--- controllers/messages-controller.js
+++ controllers/messages-controller.js
@@ -15,7 +15,7 @@ const validateUser = [
 ];
 
 async function getUsernames(req, res) {
-  const messages = await setTimeout(db.getAllUsers(), 10000);
+  const messages = await setTimeout(db.getAllUsers(), 20000);
 
   res.render("index.ejs", { title: "Mini Message Board", messages: messages });
 }
